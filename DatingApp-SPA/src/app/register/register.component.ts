f87ee4944c906416e8ea8eb610a2e04232910226
unknown <jjuracic1@gmail.com>
2018-09-10 13:03:33 +0200

    End of section 5

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
   // @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
-  
+
   model: any = {};
 
   constructor(private authService: AuthService) { }
